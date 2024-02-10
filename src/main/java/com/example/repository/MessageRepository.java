af217610b72022daf78caf5819b7a48dfb2e4186
Jon Sundin <sundinjon@gmail.com>
2024-02-10 02:29:10 +0000

    find messages by account id custom query

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,9 +1,14 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.entity.Message;
 
-public interface MessageRepository extends JpaRepository<Message, Integer> {
+import java.util.List;
 
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query("SELECT m FROM Message m WHERE m.posted_by = :account_id")
+    List<Message> findMessagesByAccountId(@Param("account_id") Integer account_id);
 }
